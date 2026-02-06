d0b03faa8415fbca756035630254be03b7a7b14c
Altobert <alberto.sanmartin@usach.cl>
2026-02-06 00:07:05 -0300

    se descomentan lineas

--- src/main/java/cl/vss/cotizador/demo/LoginController.java
+++ src/main/java/cl/vss/cotizador/demo/LoginController.java
@@ -25,12 +25,12 @@ public class LoginController {
         TextField txtCorreo = new TextField();
         txtCorreo.setPromptText("Correo electrónico");
         // 👨‍💻 Credenciales por defecto para desarrollo
-        txtCorreo.setText("albertocarloss@gmail.com");
+        // txtCorreo.setText("albertocarloss@gmail.com");
 
         PasswordField txtPassword = new PasswordField();
         txtPassword.setPromptText("Contraseña");
         // 👨‍💻 Credenciales por defecto para desarrollo
-        txtPassword.setText("Asm*031244");
+        // txtPassword.setText("Asm*031244");
 
         Button btnLogin = new Button("Ingresar");
         Label lblMensaje = new Label();
